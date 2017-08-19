51992f1117e15f86ff31dec2550fff4bbb9796af
vivshar <viveksharma7376@gmail.com>
2017-08-19 10:17:25 +0530

    1

--- src/main/java/com/vivshar/TeamB/DAOImpl.java
+++ src/main/java/com/vivshar/TeamB/DAOImpl.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DAOImpl {
 
@@ -19,22 +20,18 @@ public class DAOImpl {
 			e1.printStackTrace();
 		}
 		Connection connection = null;
-		try {
-			/*connection = DriverManager.getConnection(
+/*		try {
+			connection = DriverManager.getConnection(
 			   "jdbc:postgresql://ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g","vkpofcuxsisuio", "02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009");
-			*/
 			
-			/*URI dbUri = new URI("postgres://vkpofcuxsisuio:02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g");
+			
+			URI dbUri = new URI("postgres://vkpofcuxsisuio:02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009@ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g");
 
 		    String username = dbUri.getUserInfo().split(":")[0];
-		    String password = dbUri.getUserInfo().split(":")[1];*/
-		    String dbUrl =  "jdbc:postgresql://ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g?sslmode=require&user=vkpofcuxsisuio&password=02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009";  /*"jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();*/
+		    String password = dbUri.getUserInfo().split(":")[1];
+		    String dbUrl =  "jdbc:postgresql://ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g?sslmode=require&user=vkpofcuxsisuio&password=02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009";  "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
 		    connection =  DriverManager.getConnection(dbUrl, "vkpofcuxsisuio", "02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009");
-			
-			
-			
-			
 			
 			
 			
@@ -52,7 +49,23 @@ public class DAOImpl {
 			// TODO Auto-generated catch block
 			
 		
-		}
+		}*/
+		
+		try {
+	        // ========>     from heroku website
+			String url = "jdbc:postgresql://ec2-50-17-217-166.compute-1.amazonaws.com:5432/ddq7urlrfunt2g?user=vkpofcuxsisuio&password=02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+	        Properties props = new Properties();
+	        props.setProperty("user", "vkpofcuxsisuio");
+	        props.setProperty("password", "02a43c5a4c1e333a2f24847cb75629d6ce8a0e05276a6ae98c246bd0af103009");
+	        props.setProperty("ssl", "true");
+	        connection = DriverManager.getConnection(url, props);
+	    } catch (SQLException e) {
+
+	        System.out.println("Connection Failed! Check output console");
+	        e.printStackTrace();
+	        return -2;
+	    }
+		
 		
 		
 		
